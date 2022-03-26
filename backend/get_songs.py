9d3a893e84f888763dd6ffd7a8af744408fb3143
Dustin Nguyen <dustinnguyen@Dustins-MacBook-Pro.local>
2022-03-26 16:10:55 -0400

    adding get songs file

---
+++ backend/get_songs.py
@@ -0,0 +1,29 @@
+import json
+import random
+import requests
+
+numOfPages = str(1)
+
+response = requests.get("http://ws.audioscrobbler.com/2.0/?method=chart.gettoptracks&api_key=db917afb597e2be5c3593ec080755de6&format=json&page=" + numOfPages)
+value = response.json()
+
+trackNo = random.randint(0, len(value["tracks"]["track"]))
+
+songName = value["tracks"]["track"][trackNo]["name"]
+artist = value["tracks"]["track"][trackNo]["artist"]["name"]
+
+print(songName)
+print(artist)
+
+#code to store response as a json, use so u dont have to call api multiple times
+# # Opening JSON file
+# with open('sample.json', 'w') as openfile:
+#     json.dump(value, openfile)
+
+
+# with open('sample.json') as json_file:
+#     value = json.load(json_file)
+#     print())
+
+
+

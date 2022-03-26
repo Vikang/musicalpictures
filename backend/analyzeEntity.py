86a73b26849b8bea79d06b369d75c32453549dc2
Eric He <eh4xv@virginia.edu>
2022-03-26 19:30:52 -0400

    main:

--- backend/analyzeEntity.py
+++ backend/analyzeEntity.py
@@ -39,25 +39,24 @@ def sample_analyze_syntax(text_content):
     for token in response.tokens:
         # Get the text content of this token. Usually a word or punctuation.
         text = token.text
-        print(u"Token text: {}".format(text.content))
-        print(
-            u"Location of this token in overall document: {}".format(text.begin_offset)
-        )
+        #print(u"Token text: {}".format(text.content))
+        #print(
+        #    u"Location of this token in overall document: {}".format(text.begin_offset)
+        #)
         # Get the part of speech information for this token.
         # Part of speech is defined in:
         # http://www.lrec-conf.org/proceedings/lrec2012/pdf/274_Paper.pdf
         part_of_speech = token.part_of_speech
         # Get the tag, e.g. NOUN, ADJ for Adjective, et al.
-        print(
-            u"Part of Speech tag: {}".format(
-                language_v1.PartOfSpeech.Tag(part_of_speech.tag).name
-            )
-        )
+        #print(
+        #    u"Part of Speech tag: {}".format(
+        #        language_v1.PartOfSpeech.Tag(part_of_speech.tag).name
+         #   )
+#)
 
         if (language_v1.PartOfSpeech.Tag(part_of_speech.tag).name == "NOUN" or language_v1.PartOfSpeech.Tag(part_of_speech.tag).name == "ADP" ):
-            final += text.content + "\n"
-    print(enities(final))
-
+            final += text.content + " "
+    return enities(final)
 
 text_content = "I don't want a friend I want my life in two Waiting to get there Waiting for you  When I'm around, slow dancing in the dark Don't follow me, you'll end up in my arms You done made up your mind I don't need no more signs Can you? Can you?  Give me reasons we should be complete You should be with him, I can't compete You looked at me like I was someone else, oh, well"
 print(sample_analyze_syntax(text_content))
--- backend/get_images.py
+++ backend/get_images.py
@@ -1,13 +1,18 @@
 import requests
-key = "AIzaSyAjBuUJuCHPtMJHU2f3Bl7gV-zAPKmKWvE"
-cx = "bbcd7060a08c5b884"
+from dotenv import load_dotenv
+import os
+load_dotenv()
+
+key = os.getenv('GIMG_KEY')
+cx = os.getenv('CX')
 def lyrictoimage(text):
     newtext = text.split()
+    res = []
     for i in newtext:
         response = requests.get("https://www.googleapis.com/customsearch/v1?key="+key+"&cx="+cx+"&q="+i+"&searchType=image&num=1")
-        print(response.status_code)
+        #print(response.status_code)
         data = response.json()
-        print(data['items'][0]['link'])
-
+        res+=[data['items'][0]['link']]
+    return res
 
 lyrictoimage("apple father job")

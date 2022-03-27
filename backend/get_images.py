0de248b8256fd834c7c7a7e534af88fada1e26fd
Dustin Nguyen <dustinnguyen@Dustins-MacBook-Pro.local>
2022-03-26 20:09:59 -0400

    remove key from file

--- backend/get_images.py
+++ backend/get_images.py
@@ -1,18 +1,11 @@
-import requests
-from dotenv import load_dotenv
 import os
-load_dotenv()
-
+import requests
 key = os.getenv('GIMG_KEY')
-cx = os.getenv('CX')
+cx = os.getenv('cx')
 def lyrictoimage(text):
     newtext = text.split()
-    res = []
     for i in newtext:
         response = requests.get("https://www.googleapis.com/customsearch/v1?key="+key+"&cx="+cx+"&q="+i+"&searchType=image&num=1")
-        #print(response.status_code)
+        print(response.status_code)
         data = response.json()
-        res+=[data['items'][0]['link']]
-    return res
-
-lyrictoimage("apple father job")
+        print(data['items'][0]['link'])
